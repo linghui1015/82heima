7f4ee328717df1904d9424a1da7e94b44523139a
Klh <2573877934@qq.com>
2019-09-01 20:46:15 +0800

    加入了elementui

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui'  // 引入UI
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 Vue.config.productionTip = false
 
 new Vue({
