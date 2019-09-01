5ee63ec34519b716444f738277f2e36712830a63
Klh <2573877934@qq.com>
2019-09-01 21:58:24 +0800

    首页一级导航

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import ElementUI from 'element-ui' 
+import ElementUI from 'element-ui' // 框架组件
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import './style/index.less' // 引入初始化样式
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+Vue.use(ElementUI) // 全局注册elementUI组件 任意位置都可以使用
 new Vue({
   router,
   render: h => h(App)
